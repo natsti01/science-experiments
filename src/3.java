4d8d37b93c8fb5e2e3a766f5e974304f5a50d69f
natsti01 <natsti01@gmx.com>
2025-03-05 14:51:56 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        System.out.println("The square root of " + num + " is " + Math.sqrt(num));
+    }
+}
