f316e01f85dc529244ec1a4e67a183b72c76c265
natsti01 <natsti01@gmx.com>
2025-03-06 16:27:20 +0000

    Add 4.java

---
+++ src/4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class MyCode {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+    }
+}
