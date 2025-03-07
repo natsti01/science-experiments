c7ddfbb5b5442f3fc2e70d868dededfb62eb12dd
natsti01 <natsti01@gmx.com>
2025-03-07 09:38:14 +0000

    Add 5.java

---
+++ src/5.java
@@ -0,0 +1,6 @@
+// Example code for a simple Java program that prints "Hello, World!" to the console
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
